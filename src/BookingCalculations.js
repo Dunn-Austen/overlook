ce2cb632c21b3259c0c5f9f903ba77b1b321ef95
Alan Turing <austeyd@gmail.com>
2019-11-03 12:23:36 -0700

    Establish starter code for BookingCalculations parent class

---
+++ src/BookingCalculations.js
@@ -0,0 +1,11 @@
+class BookingCalculations {
+  constructor(bookings, rooms) {
+    this.bookings = bookings;
+    this.rooms = rooms
+  }
+
+ //Takes is either id or date (finds bookings for User or For Day)
+
+}
+
+export default BookingCalculations;
