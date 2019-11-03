6f6265aa70ad19e154c991960c2746432cb4531e
Alan Turing <austeyd@gmail.com>
2019-11-03 12:24:56 -0700

    Add findbookings method to be inherited

--- src/BookingCalculations.js
+++ src/BookingCalculations.js
@@ -5,6 +5,18 @@ class BookingCalculations {
   }
 
  //Takes is either id or date (finds bookings for User or For Day)
+  findBookings(metric) {
+    let bookingsForMetric = this.bookings.filter(booking => {
+      if (typeof(metric) === 'number') {
+        return booking.userID === metric
+      } else {
+        return booking.date === metric
+      }
+    })
+
+    return bookingsForMetric
+  }
+
 
 }
 
