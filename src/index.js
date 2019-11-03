3b82a9b4a33b73f981bfee49050bd4b99976c596
Alan Turing <austeyd@gmail.com>
2019-11-02 21:32:32 -0600

    Add catch statements to getch requests

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 
 // An example of how you import jQuery into a JS file if you use jQuery in that file
 import $ from 'jquery';
-
+import Customer from "./Customer";
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 
@@ -14,15 +14,18 @@ console.log('This is the JavaScript entry file - your code begins here.');
 
 const userData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
   .then(response => response.json())
-  .then(data => data.users);
+  .then(data => data.users)
+  .catch(error => console.log('usersData error'));
 
 const roomData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms')
   .then(roomResponse => roomResponse.json())
-  .then(data => data.rooms);
+  .then(data => data.rooms)
+  .catch(error => console.log('roomsData error'));
 
 const bookingData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')
   .then(userResponse => userResponse.json())
-  .then(data => data.bookings);
+  .then(data => data.bookings)
+  .catch(error => console.log('bookingsData error'));
 
 // username: manager
 // password: overlook2019
@@ -52,6 +55,8 @@ $('.manager-submit').on('click', function() {
   }
 });
 
+//Consider converting to if statement sequences and incorporating event.preventDefault()
+//  if the forms start causing problems
 $('.user-submit').on('click', function() {
   if ($('#user-input').val() === 'sampleName' && $('#user-password').val() === 'overlook2019') {
     $('.user-section').toggle();
@@ -60,3 +65,22 @@ $('.user-submit').on('click', function() {
     //error
   }
 });
+
+
+
+
+//// Notes on class methods
+// -total rooms Available Today
+// -Total revenue for today’s date
+// -Any room bookings for specific USER (past or present/upcoming)
+// -total spent by user all time
+// -show a list of room details for only available rooms on date X
+// -show available rooms by roomtype property
+// -be able to select a room for booking (POST REQUEST THE BOOKING)
+// -In the event that no rooms are available for the date/roomType selected, display
+//       a message fiercely apologizing to the user and asking them to adjust their room search
+// (manager)
+// -search for user by name
+// -view user name, all bookings, and total spent
+// -Add a room booking for that user
+// -Delete any upcoming room bookings for that user (they cannot delete a booking from the past) (DELETE REQUEST)
