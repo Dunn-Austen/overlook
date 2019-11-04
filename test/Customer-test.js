011584b730975b74a9d0c092340ff9ac9c76cc21
Alan Turing <austeyd@gmail.com>
2019-11-03 17:23:38 -0700

    Add spy tests for inherited methods in customer class

--- test/Customer-test.js
+++ test/Customer-test.js
@@ -1,6 +1,9 @@
 import chai from 'chai';
 const expect = chai.expect;
 
+import spies from 'chai-spies';
+chai.use(spies);
+
 import bookingsData from '../data/bookings-test-data';
 import roomsData from '../data/rooms';
 
@@ -40,6 +43,91 @@ describe('Customer', () => {
       'single room',
       'residential suite',
       'single room',
+      'residential suite',
+      'suite',
+      'single room'
+    ]);
+  });
+
+  it('should find a list of available room types by date by inheriting findBookings', () => {
+    chai.spy.on(customer, 'findBookings', () => {
+      return [
+        {
+        id: 1572293130156,
+        userID: 19,
+        date: "2019/11/06",
+        roomNumber: 18,
+        roomServiceCharges: [ ]
+        },
+
+        {
+        id: 1572293130160,
+        userID: 16,
+        date: "2019/11/06",
+        roomNumber: 7,
+        roomServiceCharges: [ ]
+        },
+
+        {
+        id: 1572293130160,
+        userID: 30,
+        date: "2019/11/06",
+        roomNumber: 11,
+        roomServiceCharges: [ ]
+        },
+
+        {
+        id: 1572293130161,
+        userID: 50,
+        date: "2019/11/06",
+        roomNumber: 14,
+        roomServiceCharges: [ ]
+        },
+
+        {
+        id: 1572293130163,
+        userID: 39,
+        date: "2019/11/06",
+        roomNumber: 21,
+        roomServiceCharges: [ ]
+        }
+      ];
+    });
+    expect(customer.findAvailableRoomsByRoomType("2019/11/06").length).to.equal(20);
+  });
+
+  it('should find a list of available room types by date by inheriting findRoomsAvailableByDate', () => {
+    chai.spy.on(customer, 'findRoomsAvailableByDate', () => {
+      return [
+        {
+        number: 1,
+        roomType: 'residential suite',
+        bidet: true,
+        bedSize: 'queen',
+        numBeds: 1,
+        costPerNight: 358.4
+        },
+
+        {
+        number: 2,
+        roomType: 'suite',
+        bidet: false,
+        bedSize: 'full',
+        numBeds: 2,
+        costPerNight: 477.38
+        },
+
+        {
+        number: 3,
+        roomType: 'single room',
+        bidet: false,
+        bedSize: 'king',
+        numBeds: 1,
+        costPerNight: 491.14
+        }
+      ];
+    });
+    expect(customer.findAvailableRoomsByRoomType("2019/11/06")).to.eql([
       'residential suite',
       'suite',
       'single room'
