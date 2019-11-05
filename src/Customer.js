83c53ceb6c4974bc67805793daddeac8644ec1fe
Alan Turing <austeyd@gmail.com>
2019-11-05 16:06:10 -0700

    Add findusernam method parameter and test for customer class

--- src/Customer.js
+++ src/Customer.js
@@ -2,8 +2,16 @@
 import BookingCalculations from "./BookingCalculations";
 
 class Customer extends BookingCalculations {
-  constructor(bookings, rooms) {
+  constructor(bookings, rooms, users) {
     super(bookings, rooms);
+    this.users = users;
+  }
+
+  findUserName(id) {
+    const userInfo = this.users.find(user => {
+      return user.id === id
+    })
+    return userInfo.name
   }
 
   findAvailableRoomsByRoomType(date) {
